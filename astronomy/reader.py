8fdff68daf6551155aa49e8911e284d26ce7de02
snpham <snpham90@gmail.com>
2020-02-22 09:54:33 -0600

    adding script for astronomy, functions for matrix algebra

---
+++ astronomy/reader.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def calc_meanmedian(file):
+    """compute mean and median from a csv file of numbers
+    """
+    data = np.loadtxt(file, delimiter=',')
+    mean = np.round(np.mean(data), 1)
+    median = np.round(np.median(data), 1)
+    return mean, median
+
+
+def mean_datasets(files):
+    """compute the mean of the indexes of multiple csv files
+    """
+    df = np.loadtxt(files[0], delimiter=',')
+    dataout = np.zeros((len(df[:,0]),len(df[0,:])))
+    ii = 0
+    while ii < len(df[:,0]):
+        jj = 0
+        while jj < len(df[0,:]):
+        datas = []
+        for csv in files:
+            data = np.loadtxt(csv, delimiter=',')
+            datas.append(data[ii,jj])
+        means = np.round(np.mean(datas), 1)
+        dataout[ii][jj] = means
+        jj += 1
+        ii += 1
+    return(dataout)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    
--- math_helpers/matrices.py
+++ math_helpers/matrices.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def rotate_x(et):
+def rotate_x(angle):
     matrix = [[1.0, 0.0, 0.0],
-              [0.0, np.cos(et), np.sin(et)],
-              [0.0, -np.sin(et), np.cos(et)]
+              [0.0, np.cos(angle), np.sin(angle)],
+              [0.0, -np.sin(angle), np.cos(angle)]]
     return matrix
 
-def rotate_y(et):
-    matrix = [[np.cos(et), 0.0, -np.sin(et)],
+
+def rotate_y(angle):
+    matrix = [[np.cos(angle), 0.0, -np.sin(angle)],
               [0.0, 1.0, 0.0],
-              [np.sin(et), 0.0, np.cos(et]]
+              [np.sin(angle), 0.0, np.cos(angle)]]
     return matrix
 
-def rotate_z(et):
-    matrix = [[np.cos(et), np.sin(et), 0.0],
-              [-np.sin(et), np.cos(et), 0.0],
+
+def rotate_z(angle):
+    matrix = [[np.cos(angle), np.sin(angle), 0.0],
+              [-np.sin(angle), np.cos(angle), 0.0],
               [0.0, 0.0, 1.0]]
     return matrix
+
+
+def transpose(m1):
+    mt = np.zeros((len(m1),len(m1)))
+    for ii in range(len(m1)):
+        for jj in range(len(m1)):
+            if ii == jj:
+                mt[ii][jj] = m1[ii][jj]
+            if ii != jj:
+                mt[ii][jj] = m1[jj][ii]
+    return mt
+
+
+def mxm(m2, m1):
+    m_out = np.zeros((len(m2),len(m1)))
+    for ii in range(3):
+        for jj in range(3):
+            m_out[ii][jj] = m2[ii][0]*m1[jj][0] + m2[ii][1]*m1[jj][1] + m2[ii][2]*m1[jj][2]
+    return m_out
+
 
 def rotate_sequence(a1, a2, a3, sequence='321'):
     if sequence == '321':
