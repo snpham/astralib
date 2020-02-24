3cb7e67c74cf81379c6a7f4f55f2f6f72495e22a
snpham <sonstarr@users.noreply.github.com>
2020-02-24 05:59:18 -0600

    fix matrix mult bug, add euler sequence function

--- math_helpers/matrices.py
+++ math_helpers/matrices.py
@@ -2,23 +2,23 @@
 import numpy as np
 
 def rotate_x(angle):
-    matrix = [[1.0, 0.0, 0.0],
-              [0.0, np.cos(angle), np.sin(angle)],
+    matrix = [[1.0,           0.0,            0.0],
+              [0.0,  np.cos(angle), np.sin(angle)],
               [0.0, -np.sin(angle), np.cos(angle)]]
     return matrix
 
 
 def rotate_y(angle):
     matrix = [[np.cos(angle), 0.0, -np.sin(angle)],
-              [0.0, 1.0, 0.0],
+              [          0.0, 1.0,            0.0],
               [np.sin(angle), 0.0, np.cos(angle)]]
     return matrix
 
 
 def rotate_z(angle):
-    matrix = [[np.cos(angle), np.sin(angle), 0.0],
+    matrix = [[ np.cos(angle), np.sin(angle), 0.0],
               [-np.sin(angle), np.cos(angle), 0.0],
-              [0.0, 0.0, 1.0]]
+              [           0.0,           0.0, 1.0]]
     return matrix
 
 
@@ -35,11 +35,21 @@ def transpose(m1):
 
 def mxm(m2, m1):
     m_out = np.zeros((len(m2),len(m1)))
-    for ii in range(3):
-        for jj in range(3):
-            m_out[ii][jj] = m2[ii][0]*m1[jj][0] + m2[ii][1]*m1[jj][1] + m2[ii][2]*m1[jj][2]
+    for ii in range(len(m2)):
+        for jj in range(len(m1)):
+            m_out[ii][jj] = m2[ii][0]*m1[0][jj] + m2[ii][1]*m1[1][jj] + m2[ii][2]*m1[2][jj]
     return m_out
 
+
+def rotate_euler(a1, a2, a3, sequence='321'):
+	if sequence == '321':
+		product1 = mxm(m2=rotate_y(a2), m1=rotate_z(a1))
+		product2 = mxm(m2=rotate_x(a3), m1=product1)
+	if sequence == '313':
+		product1 = mxm(m2=rotate_y(a2), m1=rotate_z(a1))
+		product2 = mxm(m2=rotate_z(a3), m1=product1)
+	return product2
+	
 
 def rotate_sequence(a1, a2, a3, sequence='321'):
     if sequence == '321':
@@ -51,3 +61,23 @@ def rotate_sequence(a1, a2, a3, sequence='321'):
                   [-np.sin(a3)*np.cos(a1)-np.cos(a3)*np.cos(a2)*np.sin(a1), -np.sin(a3)*np.sin(a1)+np.cos(a3)*np.cos(a2)*np.cos(a1), np.cos(a3)*np.sin(a2)],
                   [np.sin(a2)*np.sin(a1), -np.sin(a2)*np.cos(a1), np.cos(a2)]]
     return matrix
+    
+    
+    
+if __name__ == '__main__':
+	b1, b2, b3 = np.deg2rad([30, -45, 60])
+	
+	brotate = rotate_euler(b1, b2, b3, '321')
+	 #print(f'BN: {brotate}')
+	
+	f1, f2, f3 = np.deg2rad([10., 25., -15.])
+	frotate = rotate_euler(f1, f2, f3, '321')
+	frot = rotate_sequence(f1, f2, f3, '321')
+	# print(f'FN: {frotate}')
+	if np.array_equal(frotate, frot):
+		print("is equal")
+	ftranspose = transpose(frotate)
+	# print(rotate_sequence(a1, a2, a3, '313'))
+	
+	matrix = mxm(brotate, ftranspose)
+	print(f'result={matrix}')
