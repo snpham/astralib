adding tests for a few functions, fix sign error in dcm2euler

math_helpers/quaternions.py
@@ -163,13 +163,13 @@ def dcm2quat(dcm):
     """converts a dcm into a quaternion set; note singularties exist
     when s1=0, use sheppard's method if possible
     :param dcm: direction cosine matrix to extract quaternions from
-    :return: quaternion set for a given dcm
-    in work
+    :return: quaternion set for a given dcm;
+             w = q*vq returns vector v in the w frame
     """
     trace = dcm[0][0] + dcm[1][1] + dcm[2][2]
     s1 = 1./2. * np.sqrt(trace + 1)
     if s1 == 0:
-        print("s1=0, singularity exists")
+        raise ValueError("s1=0, singularity exists")
     b1 = (dcm[1][2]-dcm[2][1]) / (4.*s1)
     b2 = (dcm[2][0]-dcm[0][2]) / (4.*s1)
     b3 = (dcm[0][1]-dcm[1][0]) / (4.*s1)
@@ -180,7 +180,8 @@ def dcm2quat_sheppard(dcm):
     """converts a dcm to quaternions based on sheppard's method;
     see pg 110, Analytical Mechanics of Space Systems
     :param dcm: direction cosine matrix to extract quaternions from
-    :return: quaternion set for a given dcm
+    :return: frame transform quaternion set for a given dcm; 
+             w = q*vq returns vector v in the w frame
     """
     trace = dcm[0][0] + dcm[1][1] + dcm[2][2]
     s1_sq = 1./4. * (1+trace)
@@ -188,7 +189,6 @@ def dcm2quat_sheppard(dcm):
     b2_sq = 1./4. * (1+2*dcm[1][1]-trace)
     b3_sq = 1./4. * (1+2*dcm[2][2]-trace)
     quats = [s1_sq, b1_sq, b2_sq, b3_sq]
-    print(quats)
     if np.argmax(quats) == 0:
         s1 = np.sqrt(s1_sq)
         b1 = (dcm[1][2]-dcm[2][1])/(4.*s1)
@@ -339,61 +339,4 @@ def quat_kde_fromw(qset, wset):
 
 if __name__ == "__main__":
 
-
-    # a1, a2, a3 = rotations.dcm_inverse(frotate, sequence='313')
-    # print(np.rad2deg(a1), np.rad2deg(a2), np.rad2deg(a3))
-    # quat = euler2quat(a1, a2 , a3, sequence='313')
-    # print(quat)
-
-    # testing qxq and transmutation
-    # q1st = [0., 1./np.sqrt(2.), 1./np.sqrt(2.), 0.]
-    # q2nd = [0.5*np.sqrt(np.sqrt(3)/2+1), -0.5*np.sqrt(np.sqrt(3)/2+1), -np.sqrt(2)/(4*np.sqrt(2+np.sqrt(3))), np.sqrt(2)/(4*np.sqrt(2+np.sqrt(3)))]
-    # # print(q1st)
-    # # print(q2nd)
-    # v_out = qxq(q1st, q2nd)
-    # v_actual = [1/(2*np.sqrt(2))*np.sqrt(3), 1/(2*np.sqrt(2))*np.sqrt(3), 1/(2*np.sqrt(2))*1, 1/(2*np.sqrt(2))*1]
-    # print(v_out)
-    # print(v_actual)
-    # dcm = quat2dcm(v_out)
-    # dcm_actual = quat2dcm(v_actual)
-    # pp(dcm)
-    # pp(dcm_actual)
-
-    #testing mrp's
-    # qset = [0.961798, -0.14565, 0.202665, 0.112505]
-    # sigmaset = quat2mrp(qset)
-    # sigmasets = quat2mrps(qset)
-    # print(sigmaset, sigmasets)
-
-    # testing quat addition, scalar mult
-    # q = [1, 0.2, 0.5, 0.4]
-    # scalar = 2
-    # q2 = qxscalar(scalar=scalar, quat=q)
-    # print(q2)
-    # s = qadd(q, q2)
-    # print(s)
-
-    # # testing quat products
-    # p1 = [3, 1, -2, 1]
-    # q1 = [2, -1, 2, 3]
-    # result = qxq2(quat1=p1, quat2=q1)
-    # print(result)
-    # result = qxq(q1st=p1, q2nd=q1)
-    # print(result)
-
-    # # quaternion conjugate
-    # print(q_conjugate(q1))
-    # print(q_norm([2, -1, 2, 3]))
-
-    # p1 = [1, 0, 0, 0]
-    # print(qvqt(p1, [1,0,0]))
-    frame_T_trn_c2ils= [7.11390093e-06, 8.66068429e-01, 4.12538558e-05, 4.99925468e-01]
-    frame_T_hrn_c2ils= [ 2.58902171e-01, -3.42011688e-05,  9.65903548e-01, -2.41405936e-05]
-    vec = [0, 0, 1]
-    result = q_operator_frame(frame_T_hrn_c2ils, vec)
-    print(result)
-    result = np.rad2deg(quat2euler(frame_T_hrn_c2ils, sequence='321'))
-    print(result)
-    dcm = quat2dcm(frame_T_hrn_c2ils)
-    axis, angle = rotations.prv_axis(dcm)
-    print(axis, np.rad2deg(angle))
+    pass

math_helpers/rotations.py
@@ -96,7 +96,7 @@ def dcm2euler(dcm, sequence='321'):
         angle3rd = np.arctan2(dcm[1][2],dcm[2][2])
     elif sequence == '313':
         angle1st = np.arctan2(dcm[2][0],-dcm[2][1])
-        angle2nd = -np.arccos(dcm[2][2])
+        angle2nd = np.arccos(dcm[2][2])
         angle3rd = np.arctan2(dcm[0][2],dcm[1][2])
     else:
         raise ValueError(f'euler sequence not yet implemented')
@@ -137,12 +137,17 @@ def axisofr(Tmatrix):
     vout[0] = Tmatrix[0][1]*Tmatrix[1][2] - (Tmatrix[1][1] -1)*Tmatrix[0][2]
     vout[1] = Tmatrix[1][0]*Tmatrix[0][2] - (Tmatrix[0][0] -1)*Tmatrix[1][2]
     vout[2] = (Tmatrix[0][0] - 1)*(Tmatrix[1][1] - 1) - Tmatrix[0][1]*Tmatrix[1][0]
-    phi = np.arccos((Tmatrix[0][0]+Tmatrix[1][1]+Tmatrix[2][2] - 1) / 2.)
+    phi = prv_angle(Tmatrix)
     return vout, phi
 
 
 def dcm_rate(omega_tilde, dcm):
-    """in work
+    """returns the DCM differential kinematic equation showing how
+    the dcm evolve over time.
+    :param omega_tilde: body angular velocity vector in skewed
+                        symmetric matrix form
+    :param dcm: direction cosine matrix of a rotation
+    :return dcm_dot = -[~w][dcm]
     """
     return -mat.mxm(omega_tilde, dcm)
 
@@ -222,10 +227,10 @@ def eulerrates_frm_wvec_o2b(aset, wvec, sequence='321'):
         matrix = [[0.0,        s(aset[2]),             c(aset[2])],
                   [0.0,        c(aset[2])*c(aset[1]), -s(aset[2])*c(aset[1])],
                   [c(aset[1]), s(aset[2])*s(aset[1]),  c(aset[2])*s(aset[1])]]
-        mxwvec = mat.mxv(m1=matrix, v1=wvec)
+        matrix = mat.mxscalar(scalar=1/c(aset[1]), m1=matrix)
     else:
         raise ValueError(f'euler sequence not yet implemented')
-    return vectors.vxscalar(scalar=1/c(aset[1]), v1=mxwvec)
+    return mat.mxv(m1=matrix, v1=wvec)
 
 
 def wvec_frm_eulerrates_n2b(aset, rates, Omega, sequence='321'):
@@ -248,38 +253,18 @@ def eulerrates_frm_wvec_n2b(aset, wvec, Omega, sequence='321'):
         term2 = vectors.vxscalar(scalar=Omega/np.cos(aset[1]), \
             v1=[np.sin(aset[1])*np.sin(aset[0]), np.cos(aset[1])*np.cos(aset[0]), np.sin(aset[0])])
     else:
-        raise ValueError(f'euler sequence not yet implemented')vdf 
+        raise ValueError(f'euler sequence not yet implemented')
     return vectors.vxadd(v1=rates_o2b, v2=-term2)
 
 
 if __name__ == "__main__":
-    pass
-    # #testing triad method
-    # v1 = [1, 0, 0]
-    # v2 = [0, 0, 1]
-    # tveci, t2i = triad(v1, v2)
-    # print(tveci)
-    # print(t2i)
-    # # bn_actual = euler2dcm(a1=np.deg2rad(30), a2=np.deg2rad(20), a3=np.deg2rad(-10), 
-    # #                            sequence='321')
-    # # v1out_a = mat.mxv(bn_actual, v1)
-    # # v2out_a = mat.mxv(bn_actual, v2)
-    # v1out = [0.8190, -0.5282, 0.2242]
-    # v2out = [-0.3138, -0.1584, 0.9362]
-    # # print(v1out, v2out)
-    # tvec, tmatrix = triad(v1out, v2out)
-    # # print(tvec)
-    # # print(tmatrix)
-    # bn = mat.mxm(m2=tmatrix, m1=t2i)
-    # print(bn)
-    # bn_error = mat.mxm(m2=bn, m1=mat.mtranspose(bn_actual))
-    # print(bn_error)
+    
+    angles = np.deg2rad((20, -10, 120))
+    axis, angle = prv_axis(euler2dcm(angles[0], angles[1], angles[2], sequence='321'))
+    print(angle, axis)
 
-    # aset = [1.0, 0.0, 0.0]
-    # rates = [1.0, 0.0, 0.0]
-    # sequence = '321'
-    # w_n2b = wvec_frm_eulerrates_n2b(aset=aset, rates=rates, Omega=0.0, sequence='321')
-    # print(w_n2b)
-    # wvec = [1.0, 0.0, 0.0]
-    # rates_n2b = eulerrates_frm_wvec_n2b(aset=aset, wvec=wvec, Omega=0.0, sequence='321')
-    # print(rates_n2b)
+    bn = [[1, 0, 0], [0, 0, 1], [0, -1, 0]]
+    dcm = mat.mxm(bn, bn)
+    print(dcm)
+    axis, angle = axisofr(dcm)
+    print(angle, axis)

tests/mat_rot_test.py
@@ -16,8 +16,8 @@ def test_mxm():
 
 
 def test_euler_rotation():
-    """321; tests rotate_x, rotate_y, rotate_z, euler2dcm,
-    dcm2euler, and euler2dcm functions
+    """321 and 313; tests rotate_x, rotate_y, rotate_z, euler2dcm,
+    euler2dcm2, dcm2euler, and euler2dcm functions
     """
     # euler angles for n2b, n2f
     b1, b2, b3 = np.deg2rad([30, -45, 60])
@@ -46,6 +46,61 @@ def test_euler_rotation():
     T_f2b = rotations.euler2dcm(a1, a2, a3, '321')
     assert np.allclose(T_f2b, T_f2b_truth)
 
+    # another dcm rotation test
+    dcm = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    dcmx = rotations.rotate_x(np.deg2rad(90))
+    dcmy = rotations.rotate_y(np.deg2rad(-90))
+    dcmy = mat.mtranspose(dcmy)
+    br = mat.mxm(dcmx, dcmy)
+    br_true = [[0.0, 0.0, -1.0],
+               [1.0, 0.0, 0.0], 
+               [0.0, -1.0, 0.0]]
+    assert np.allclose(br, br_true)
+
+    # manual composite rotation
+    sqrt = np.sqrt
+    bn = [[1/3, 2/3, -2/3], [0, 1/sqrt(2), 1/sqrt(2)], [4/(3*sqrt(2)), -1/(3*sqrt(2)), 1/(3*sqrt(2))]]
+    fn = [[3/4, -2/4, sqrt(3)/4],[-1/2, 0, sqrt(3)/2], [-sqrt(3)/4, -2*sqrt(3)/4, -1/4]]
+    nf = mat.mtranspose(fn)
+    bf = mat.mxm(bn, nf)
+    bf_true = [[-0.37200847, -0.74401694, -0.55502117], 
+               [-0.04736717, 0.61237244, -0.78914913], 
+               [0.92701998, -0.26728038, -0.26304971]]
+    assert np.allclose(bf, bf_true)
+
+    # testing 313 sequence
+    angles = np.deg2rad((10, 20, 30))
+    dcm1 = rotations.euler2dcm(angles[0], angles[1], angles[2], sequence='321')
+    eulers = np.rad2deg(rotations.dcm2euler(dcm1, sequence='313'))
+    eulers_true = [40.64234205, 35.53134776, -36.05238873]
+    assert np.allclose(eulers, eulers_true)
+
+    # testing 313 sequence with composite rotation
+    ang2 = np.deg2rad((-5, 5, 5))
+    dcm_BN = rotations.euler2dcm2(angles[0], angles[1], angles[2], sequence='321')
+    dcm_RN = rotations.euler2dcm2(ang2[0], ang2[1], ang2[2], sequence='321')
+    dcm_NR = matrices.mtranspose(dcm_RN)
+    dcm = matrices.mxm(dcm_BN, dcm_NR)
+    eulers = np.rad2deg(rotations.dcm2euler(dcm, sequence='321'))
+    eulers_true = [13.22381821, 16.36834338, 23.61762825]
+    assert np.allclose(eulers, eulers_true)
+
+
+def test_dcm_rates():
+    """tests dcm_rate function
+    """
+    bn = [[-0.87097, 0.45161, 0.19355], 
+          [-0.19355, -0.67742, 0.70968], 
+          [0.45161, 0.58065, 0.67742]]
+    wbn = [0.1, 0.2, 0.3]
+    wbn_tl = matrices.skew_tilde(wbn)
+    rates = rotations.dcm_rate(omega_tilde=wbn_tl, dcm=bn)
+    rates_true = [[-0.148387, -0.319356,  0.07742], 
+                  [ 0.306452, -0.077418,  0.009677], 
+                  [-0.154839,  0.158064, -0.032258]]
+    assert np.allclose(rates, rates_true)
+
+
 
 def test_prv():
     """tests principal rotation axis and angle 
@@ -65,6 +120,26 @@ def test_prv():
     evec_truth =(-0.532035, 0.740302, 0.410964)
     assert np.allclose(phi_deg, 31.7762)
     assert np.allclose(evec, evec_truth)
+
+    # second prv axis test
+    dcm = [[0.925417, 0.336824, 0.173648],
+           [0.0296956, -0.521281, 0.852869],
+           [0.377786, -0.784102, -0.492404]]
+    e, angle = rotations.prv_axis(dcm)
+    e_true = [0.975550, 0.121655, 0.183032]
+    angle_true = 2.146152
+    assert np.allclose(e, e_true)
+    assert np.allclose(angle, angle_true)
+
+    # third prv axis test
+    angles = np.deg2rad((20, -10, 120))
+    e, angle = rotations.prv_axis(rotations.euler2dcm( \
+            angles[0], angles[1], angles[2], sequence='321'))
+    e_true = [0.975550, 0.121655, 0.183032]
+    angle_true = 2.146152
+    assert np.allclose(e, e_true)
+    assert np.allclose(angle, angle_true)
+
 
 
 def test_triad_method():

tests/quaternions_test.py
@@ -48,7 +48,10 @@ def test_qvq():
     assert np.allclose(wvec, wvec_truth)    
 
 
-def test_sheppard():
+def test_quat2dcm():
+    """tests quat2dcm and sheppards method
+    """
+    # sheppard
     # given a dcm
     T_dcm = [[ 0.892539,  0.157379, -0.422618], 
              [-0.275451,  0.932257, -0.234570], 
@@ -57,6 +60,44 @@ def test_sheppard():
     quat = quaternions.dcm2quat_sheppard(T_dcm)
     quat_truth = (0.961798, -0.145650, 0.202665, 0.112505)
     assert np.allclose(quat, quat_truth)
+
+    # quat2dcm basic
+    qset = [0.235702, 0.471405, -0.471405, 0.707107]
+    dcm = quaternions.quat2dcm(qset)
+    dcm_true = [[-0.444444, -0.111112, 0.888889], 
+                [-0.777778, -0.444444, -0.444445], 
+                [0.444445, -0.888889, 0.111110]]
+    assert np.allclose(dcm, dcm_true)
+
+    # 2nd quat2dcm and sheppard test
+    bn = [[-0.529403, -0.467056, 0.708231], 
+          [-0.474115, -0.529403, -0.703525], 
+          [0.703525, -0.708231, 0.0588291]]
+    epset = quaternions.dcm2quat(bn)
+    epset_true = [0.0024254, 0.4850696, -0.4850696, 0.7276048]
+    assert np.allclose(epset, epset_true, rtol=0, atol=1e-02) # not accurate
+    epset = quaternions.dcm2quat_sheppard(bn)
+    assert np.allclose(epset, epset_true,rtol=0, atol=1e-06)
+
+    # 3rd quat2dcm/sheppard test
+    q_bn = [0.774597,0.258199,0.516398,0.258199]
+    q_fb = [0.359211,0.898027,0.179605,0.179605]
+    dcm_bn = quaternions.quat2dcm(q_bn)
+    dcm_fb = quaternions.quat2dcm(q_fb)
+    dcm_fn = matrices.mxm(dcm_fb, dcm_bn)
+    q_fn = quaternions.dcm2quat_sheppard(dcm_fn)
+    q_fn = quaternions.dcm2quat(dcm_fn)
+    q_fn_true = [0.0927449, -0.8347526, -0.5101265, 0.1855009]
+    assert np.allclose(q_fn, q_fn_true)
+
+
+def test_euler2quat():
+    """tests euler2quat function
+    """
+    aset = np.deg2rad([20, 10, -10])
+    quat = quaternions.euler2quat(aset[0], aset[1], aset[2], sequence='321')
+    quat_true = [0.9760079, -0.1005818, 0.0704281, 0.1798098]
+    assert np.allclose(quat, quat_true)
 
 
 def test_mrps():